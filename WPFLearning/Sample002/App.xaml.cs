f8545da251afd76594241f418e1a70bd8671fe8a
vinoj2010 <vinoj.colossal@gmail.com>
2016-03-18 22:56:34 +0530

    Checking sample002 on Resources

--- WPFLearning/Sample002/App.xaml.cs
+++ WPFLearning/Sample002/App.xaml.cs
@@ -6,17 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Sample001
+namespace Sample002
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            MessageBox.Show("starting application...");
-            base.OnStartup(e);
-        }
     }
 }
